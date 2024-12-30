6f67ee87835730a64c3052019fb6be686c78849e
jcstlo <62954657+jcstlo@users.noreply.github.com>
2024-12-30 14:33:50 -0800

    Create initial Pen model

--- pens/models.py
+++ pens/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Pen(models.Model):
+    name = models.CharField(max_length=200)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    creator = models.CharField(max_length=200, blank=True)
+    nib_size = models.CharField(max_length=30, blank=True)
+    purchase_date = models.DateField(null=True, blank=True)
+    color = models.CharField(max_length=50, blank=True)
+    manufacture_year = models.PositiveSmallIntegerField(null=True, blank=True)
+    purchase_location = models.CharField(max_length=100, blank=True)
+    description = models.CharField(max_length=300, blank=True)
+    favorite = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
