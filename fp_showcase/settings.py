c551f88cc53737a2472287bda2dd20631f4d38ab
jcstlo <62954657+jcstlo@users.noreply.github.com>
2024-12-31 13:21:26 -0800

    Implement login/logout functionality

--- fp_showcase/settings.py
+++ fp_showcase/settings.py
@@ -136,3 +136,6 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
