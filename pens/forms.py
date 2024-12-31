0ef561926e769f3282b3e8dbe160ae31c8df11c5
jcstlo <62954657+jcstlo@users.noreply.github.com>
2024-12-30 19:41:52 -0800

    Add better field name in add pen form

--- pens/forms.py
+++ pens/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.forms import ModelForm
+from django.utils.translation import gettext_lazy as _
 
 from .models import Pen
 
@@ -24,4 +25,7 @@ class PenForm(ModelForm):
         )
         widgets = {
             "purchase_date": DateInput(),
+        }
+        labels = {
+            "creator": _("Brand/company/creator"),
         }
