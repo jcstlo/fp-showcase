fa679e77bfa2bbf343a484c3da936dd84a8bf422
jcstlo <62954657+jcstlo@users.noreply.github.com>
2025-01-01 14:50:05 -0800

    Make minor improvements to add pen route

--- pens/urls.py
+++ pens/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import AddPenView
 
 urlpatterns = [
-    path("add", AddPenView.as_view(), name="add_pen"),
+    path("add/", AddPenView.as_view(), name="add_pen"),
 ]
--- pens/views.py
+++ pens/views.py
@@ -8,7 +8,7 @@ from .forms import PenForm
 class AddPenView(LoginRequiredMixin, CreateView):
     template_name = "add_pen.html"
     form_class = PenForm
-    success_url = reverse_lazy("add_pen")  # TODO: change to profile or home page
+    success_url = reverse_lazy("home")
 
     def form_valid(self, form):
         form.instance.owner = self.request.user
