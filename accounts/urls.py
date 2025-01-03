5baaec619cb420dbc84ffdc845e137f813b1b9dc
jcstlo <62954657+jcstlo@users.noreply.github.com>
2025-01-03 14:04:25 -0800

    Create profile settings page (UserUpdateView)

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import SignUpView
+from .views import SignUpView, UserUpdateView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("settings/", UserUpdateView.as_view(), name="settings"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,49 @@
 from .forms import CustomUserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext as _
+from django.http import Http404
 
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+
+class UserUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = get_user_model()
+    fields = [
+        "profile_picture",
+    ]
+    template_name = "profile_settings.html"
+    success_url = reverse_lazy("home")
+
+    # override SingleObjectMixin's get_object() to return the current user object instead,
+    # so user ID doesn't need to be exposed in URL
+    def get_object(self, queryset=None):
+        if queryset is None:
+            queryset = self.get_queryset()
+
+        pk = self.request.user.pk
+        if pk is not None:
+            queryset = queryset.filter(pk=pk)
+        else:
+            raise AttributeError(
+                "Error with retrieving user for profile settings page."
+            )
+
+        try:
+            obj = queryset.get()
+        except queryset.model.DoesNotExist:
+            raise Http404(
+                _("No %(verbose_name)s found matching the query")
+                % {"verbose_name": queryset.model._meta.verbose_name}
+            )
+        return obj
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj == self.request.user
