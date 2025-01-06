987a1d2be2c30edc841561e713263929a7280fc3
jcstlo <62954657+jcstlo@users.noreply.github.com>
2025-01-06 13:45:56 -0800

    Update ALLOWED_HOSTS for local dev env

--- fp_showcase/settings.py
+++ fp_showcase/settings.py
@@ -29,7 +29,12 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
-ALLOWED_HOSTS = [env.str("DJANGO_ALLOWED_HOSTS", default="")]
+ALLOWED_HOSTS = [
+    env.str("DJANGO_ALLOWED_HOSTS", default=""),
+    ".localhost",
+    "127.0.0.1",
+    "[::1]",
+]
 
 # Application definition
 
