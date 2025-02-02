e0c2ac87ec54e4dec52cc59432b1c1cfd006905f
jcstlo <62954657+jcstlo@users.noreply.github.com>
2025-02-01 16:19:42 -0800

    Add unit tests for signup functionality

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,52 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your tests here.
+
+class UserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username="joseph",
+            email="joseph@email.com",
+            password="testpass123",
+        )
+        self.assertEqual(user.username, "joseph")
+        self.assertEqual(user.email, "joseph@email.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username="superuser",
+            email="superuser@email.com",
+            password="testpass123",
+        )
+        self.assertEqual(admin_user.username, "superuser")
+        self.assertEqual(admin_user.email, "superuser@email.com")
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+
+
+class SignupPageTests(TestCase):
+    username = "newuser"
+    email = "newuser@email.com"
+
+    def setUp(self):
+        url = reverse("signup")
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, "registration/signup.html")
+        self.assertContains(self.response, "Sign Up")
+        self.assertNotContains(self.response, "This should not be on the page")
+
+    def test_signup_form(self):
+        new_user = get_user_model().objects.create_user(self.username, self.email)
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, self.username)
+        self.assertEqual(get_user_model().objects.all()[0].email, self.email)
