88776914c153f6467110f56dc9475f3dfb77015f
jcstlo <62954657+jcstlo@users.noreply.github.com>
2025-02-01 17:21:25 -0800

    Add unit test for homepage, conditional on logged in user

--- pages/tests.py
+++ pages/tests.py
@@ -1,9 +1,10 @@
-from django.test import SimpleTestCase
+from django.test import TestCase
 from django.urls import reverse, resolve
 from .views import HomePageView
+from django.contrib.auth import get_user_model
 
 
-class HomepageTests(SimpleTestCase):
+class HomepageTests(TestCase):
     def setUp(self):
         url = reverse("home")
         self.response = self.client.get(url)
@@ -23,3 +24,21 @@ class HomepageTests(SimpleTestCase):
     def test_homepage_url_resolves_homepageview(self):
         view = resolve("/")
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
+
+    def test_homepage_view_for_logged_in_user(self):
+        new_user = get_user_model().objects.create_user(
+            username="testuser",
+            email="test@email.com",
+            password="testpass123",
+        )
+        self.client.login(username="testuser", password="testpass123")
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "Hello,")
+        self.assertContains(response, "testuser")
+        self.assertNotContains(response, "Show your collection of fountain pens")
+
+    def test_homepage_view_for_logged_out_user(self):
+        self.client.logout()
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "Show your collection of fountain pens")
+        self.assertNotContains(response, "Hello,")
